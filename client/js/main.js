172f070f5ea04c51140174e73cdaf26527dc3945
SharkFin - SharkFin Productions <sharkfinpro@hotmail.com>
2016-10-08 19:32:36 -0700

    Update

---
+++ client/js/main.js
@@ -0,0 +1,358 @@
+var sketchProc = function(processingInstance) {
+    with(processingInstance) {
+        size(window.innerWidth, window.innerHeight-3.25);
+        frameRate(60); //Set The Frame Rate
+        textAlign(CENTER, CENTER);
+        var l = function() {
+            return this.Function("gflink", "var f=document.createElement('link');f.setAttribute('rel','stylesheet');f.setAttribute('type','text/css');f.setAttribute('href',gflink);document.head.appendChild(f);");
+        }();
+        l("./css/ubuntu.css");
+        textFont(createFont("Ubuntu"));
+        textSize(20);
+
+        var socket = io.connect();
+        var username =  [];
+        var gameArea =  document.getElementById("gameArea");
+        var chatForm =  document.getElementById("chatForm");
+        var message =  document.getElementById("message");
+        var messages =  document.getElementById("messages");
+        var chat = document.getElementById("chat");
+        var users = [];
+        var myId=0;
+        var myIdAssigned=false;
+        var myNum=0;
+        var myNumAssigned=false;
+
+        var playing = false;
+        var hideGrid = false;
+
+        /** Colors **/
+        var colors = {
+            square: color(255,232,105),
+            triangle: color(252,118,119),
+            pentagon: color(118,141,252),
+            tank_red: color(241,78,84),
+            tank_blue: color(0,178,225),
+            tank_green: color(76,233,153),
+            tank_purple: color(191,127,245),
+            tank_barrel: color(153,153,153)
+        };
+
+        var textOutline = function(t, x, y, w, h, fc, sc, o) {
+            fill(sc);
+            o = max(o, 1);
+            for (var a = 0; a < 360; a += 360 / (2 * PI * o)) {
+                if (w > 0 && h > 0) {
+                    text(t, x + cos(a) * o, y + sin(a) * o, w, h);
+                } else {
+                    text(t, x + cos(a) * o, y + sin(a) * o);
+                }
+            }
+            fill(fc);
+            if (w > 0 && h > 0) {
+                text(t, x, y, w, h);
+            } else {
+                text(t, x, y);
+            }
+        };
+
+        /** World **/
+        var world = {
+            w: 0, //World Width
+            h: 0 //World Height
+        };
+
+        /** Enemies **/
+        var squares = [];
+        var triangles = [];
+        var pentagons = [];
+
+        var Square = function(x, y, r) {
+            this.pos = new PVector(x, y); //Its Position
+            this.r = r; //Its Starting Rotation
+            this.d = 35; //Its Diameter
+        };
+        Square.prototype.display = function() {
+            pushMatrix();
+            translate(this.pos.x, this.pos.y);
+            rotate(this.r);
+            stroke(85);
+            strokeWeight(3);
+            fill(colors.square);
+            rect(-this.d / 2, -this.d / 2, this.d, this.d);
+            popMatrix();
+        };
+
+        var Triangle = function(x, y, r) {
+            this.pos = new PVector(x, y); //Its Position
+            this.r = r; //Its Starting Rotation
+            this.d = 20; //Its Diameter
+        };
+        Triangle.prototype.display = function() {
+            pushMatrix();
+            translate(this.pos.x, this.pos.y);
+            rotate(this.r);
+            stroke(85);
+            strokeWeight(3);
+            fill(colors.triangle);
+            triangle(0, 0 - this.d / 1.25, 0 - this.d, 0 + this.d, 0 + this.d, 0 + this.d);
+            popMatrix();
+        };
+
+        var Pentagon = function(x, y, r) {
+            this.pos = new PVector(x, y); //Its Position
+            this.r = r; //Its Starting Rotation
+            this.d = 60; //Its Diameter
+        };
+        Pentagon.prototype.display = function() {
+            pushMatrix();
+            translate(this.pos.x, this.pos.y);
+            rotate(this.r);
+            stroke(85);
+            strokeWeight(3);
+            fill(colors.pentagon);
+            beginShape();
+            vertex(0, 0 - this.d / 2);
+            vertex(0 + this.d / 2, 0 - this.d / 8);
+            vertex(0 + this.d / 3, 0 + this.d / 2);
+            vertex(0 - this.d / 3, 0 + this.d / 2);
+            vertex(0 - this.d / 2, 0 - this.d / 8);
+            vertex(0, 0 - this.d / 2);
+            endShape();
+            popMatrix();
+        };
+
+        if(chatForm.addEventListener){
+            chatForm.addEventListener("submit", function(e){
+                e.preventDefault();
+                socket.emit('send message', message.value);
+                message.value = '';
+            }, false);  //Modern browsers
+        }else if(chatForm.attachEvent){
+            chatFormele.attachEvent('onsubmit', function(e){
+                e.preventDefault();
+                socket.emit('send message', message.value);
+                message.value = '';
+            });            //Old IE
+        }
+
+        socket.on('get users', function(data){
+            var stuffs=[];
+            for(var i=0; i<data.length; i++){
+                stuffs.push({x: data[i].x, y: data[i].y, name: data[i].name, id: data[i].id});
+            }
+            users=stuffs;
+            var num=0;
+            for(var i=0; i<users.length; i++){
+                if(users[i].id===myId){
+                  myNum = num;
+                }
+                num+=1;
+            }
+            myNumAssigned=true;
+        });
+        socket.on('get id', function(data){
+            if(!myIdAssigned){
+                myId=data;
+                myIdAssigned=true;
+            }
+        });
+        socket.on('get messages', function(data){
+            var html = '';
+            for(var i=0; i<data.length; i++){
+                if(data[i].to==='all'||data[i].to===myId){
+                    html += '<div class="well"><strong>'+data[i].user+':</strong> '+data[i].msg+'</div>';
+                }
+            }
+            messages.innerHTML = html;
+            messages.scrollTop = messages.scrollHeight;
+        });
+
+        socket.on('update world', function(data){
+            world.w = data.w;
+            world.h = data.h;
+        });
+        socket.on('update enemies', function(s, t, p){
+            var stuff=[];
+            for(var i=0; i<s.length; i++){
+                stuff.push(new Square(s[i].x, s[i].y, s[i].r));
+            }
+            squares=stuff;
+            var stuff=[];
+            for(var i=0; i<t.length; i++){
+                stuff.push(new Triangle(t[i].x, t[i].y, t[i].r));
+            }
+            triangles=stuff;
+            var stuff=[];
+            for(var i=0; i<p.length; i++){
+                stuff.push(new Pentagon(p[i].x, p[i].y, p[i].r));
+            }
+            pentagons=stuff;
+        });
+
+        socket.on('banned', function(data){
+            if(data===myIp){
+                window.location="./banned.html";
+            }
+        });
+
+        var keys = {};
+        var keyPressed = function() {
+            keys[keyCode] = true;
+        };
+        var keyReleased = function() {
+            keys[keyCode] = false;
+        };
+        var mapCamera = {
+            x: 0,
+            y: 0,
+            right: -world.w,
+            bottom: -world.h,
+            run: function() {
+                for(var i=0; i<users.length; i++){
+                    if(users[i].id===myId){
+                        this.x = users[i].x;
+                        this.y = users[i].y;
+                        this.right = -world.w;
+                        this.bottom = -world.h;
+                        this.x = constrain(this.x + (width / 2 - users[i].x - this.x), this.right, this.left);
+                        this.y = constrain(this.y + (height / 2 - users[i].y - this.y), this.bottom, this.top);
+                        translate(this.x, this.y);
+                    }
+                }
+            }
+        };
+        var minimap =  {
+            x: width-135,
+            y: height-135,
+            ax: 0,
+            ay: 0,
+            run: function() {
+                if (this.x !== width - 135 || this.y !== height - 135) {
+                    this.x = width - 135;
+                    this.y = height - 135;
+                }
+                if(myNumAssigned){
+                    this.ax = 115 * users[myNum].x / world.w;
+                    this.ay = 115 * users[myNum].y / world.h;
+                }
+                stroke(100);
+                strokeWeight(5);
+                fill(207, 207, 207, 200);
+                rect(this.x, this.y, 125, 125);
+                strokeWeight(5);
+                stroke(114);
+                pushMatrix();
+                translate(this.ax + (width - 130), this.ay + (height - 130));
+                rotate(atan2(mouseY - height / 2, mouseX - width / 2));
+                fill(0);
+                triangle(10, 0, -1, -2.5, -1, 2.5);
+                popMatrix();
+            }
+        };
+
+        var draw = function() {
+            size(window.innerWidth, window.innerHeight-3.25);
+            if(playing){
+                background(185);
+                pushMatrix();
+                mapCamera.run();
+                fill(205);
+                noStroke();
+                rect(0,0,world.w,world.h);
+                if(!hideGrid){
+                    stroke(170);
+                    strokeWeight(1);
+                    for (var w = 0-width; w < world.w+width; w += 22.5) {
+                        line(w, 0, w, world.w);
+                    }
+                    for (var h = 0-height; h < world.h+height; h += 22.5) {
+                        line(0, h, world.h, h);
+                    }
+                }
+                for(var i=0; i<squares.length; i++){
+                    squares[i].display();
+                }
+                for(var i=0; i<triangles.length; i++){
+                    triangles[i].display();
+                }
+                for(var i=0; i<pentagons.length; i++){
+                    pentagons[i].display();
+                }
+                textSize(20);
+                for(var i=0; i<users.length; i++){
+                    if(users[i].id===myId){
+                        fill(colors.tank_blue);
+                    } else {
+                        fill(colors.tank_red);
+                    }
+                    ellipse(users[i].x, users[i].y, 35, 35);
+                    fill(255, 255, 255);
+                    text(users[i].name, users[i].x, users[i].y);
+                }
+                popMatrix();
+                minimap.run();
+                if(keys[UP]){
+                    socket.emit('move up', 2.5);
+                }
+                if(keys[DOWN]){
+                    socket.emit('move down', 2.5);
+                }
+                if(keys[LEFT]){
+                    socket.emit('move left', 2.5);
+                }
+                if(keys[RIGHT]){
+                    socket.emit('move right', 5);
+                }
+            } else {
+                background(124,124,124);
+                textSize(16);
+                //textOutline("Game mode: " + gameMode, width / 2, 20, 0, 0, color(255), color(0), 2);
+                textSize(20);
+                textOutline("This is the tale of...", width / 2, height / 2 - 35, 0, 0, color(255), color(0), 2);
+                fill(238);
+                stroke(0);
+                strokeWeight(4);
+                rect(width / 2 - (325 / 2), height / 2 - (40 / 2), 325, 40);
+                textAlign(LEFT, CENTER);
+                textSize(30);
+                if (round(frameCount / 40) % 2 === 0) {
+                    textOutline(username.join("") + "|", width / 2 - (320 / 2), height / 2, 0, 0, color(0));
+                } else {
+                    textOutline(username.join(""), width / 2 - (320 / 2), height / 2, 0, 0, color(0));
+                }
+                textAlign(CENTER, CENTER);
+                textSize(11);
+                textOutline("(press enter to spawn)", width / 2, height / 2 + 30, 0, 0, color(255), color(0), 1.25);
+                if (keys[ENTER]) {
+                    socket.emit('new user', username.join(""), myIp, function(data){
+                        if(data){
+                            //do this next
+                        }
+                    });
+                    playing = true;
+                    chat.style.visibility = 'visible';
+                }
+            }
+        };
+        var keyTyped = function() {
+          if (!playing) {
+              if (key.code !== BACKSPACE && username.length < 15 && !keys[ENTER]) {
+                  username.push(key);
+              }
+              if (key.code === BACKSPACE) {
+                  username.pop();
+              }
+          }
+          if(playing){
+              if(key.code===104 && !hideGrid){ // H key for hiding grid
+                  hideGrid = true;
+              } else if(key.code===104 && hideGrid){
+                  hideGrid = false;
+              }
+          }
+        };
+    }
+};
+var canvas = document.getElementById("canvas");
+var processingInstance = new Processing(canvas, sketchProc);
---
+++ server.js
@@ -0,0 +1,322 @@
+var http = require('http');
+var path = require('path');
+
+var socketio = require('socket.io');
+var express = require('express');
+
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+var users = [];
+var connections = [];
+var messages = [];
+var Id=1;
+var debug=false;
+var chatBanList=[];
+var banList=[];
+
+var updateMessages;
+var updateUsernames;
+var updateWorld;
+var updatePositions;
+var ban;
+var updateEnemies;
+var updatingStarted = false;
+
+var squares = [];
+var triangles = [];
+var pentagons = [];
+var bullets = [];
+
+/** World **/
+var world = {
+    w: 6000, //World Width
+    h: 6000, //World Height
+    minimumSquares: 75, //Minimum Amount Of Squares
+    minimumTriangles: 50, //Minimum Amount Of Triangles
+    minimumPentagons: 10, //Minimum Amount Of Pentagons
+    maxSquares: 175, //Maximum Amount Of Squares
+    maxTriangles: 125, //Maximum Amount Of Triangles
+    maxPentagons: 75 //Maximum Amount Of Pentagons
+};
+
+io.on('connection', function (socket) {
+    connections.push(socket);
+    if(debug){ console.log('Connected: %s users connected', connections.length); }
+
+    // Disconnect
+    socket.on('disconnect', function(data){
+        if(socket.username !== undefined){
+            users.splice(users.indexOf(socket.username), 1);
+            updateUsernames();
+        }
+        connections.splice(connections.indexOf(socket), 1);
+        if(debug){ console.log('Connected: %s users connected', connections.length); }
+    });
+
+    //New User
+    socket.on('new user', function(data, Ip, callback){
+        callback(true);
+        //socket.username = {name: data, x: Math.floor(Math.random() * (world.w-100 - 100 + 1) + 100), y: Math.floor(Math.random() * (world.h-100 - 100 + 1) + 100), id: Id, ip: Ip};
+        socket.username = {name: data, x: 100, y:100, id: Id, ip: Ip};
+        users.push(socket.username);
+        updateUsernames();
+        updateWorld();
+        updateMessages();
+        Id+=1;
+        ban();
+    });
+
+    //Send Message
+    socket.on('send message', function(data){
+        var cc=true;
+        for(var i=0; i<chatBanList.length; i++){
+            if(socket.username.id===chatBanList[i]){
+                cc=false;
+            }
+        }
+        if(cc){
+            var message = {msg: data, user: socket.username.name, to: "all"};
+            messages.push(message);
+            updateMessages();
+        }
+    });
+
+    //Movement
+    socket.on('move right', function(speed, callback){
+        users[users.indexOf(socket.username)].x+=speed;
+        updatePositions();
+    });
+    socket.on('move left', function(speed, callback){
+        users[users.indexOf(socket.username)].x-=speed;
+        updatePositions();
+    });
+    socket.on('move up', function(speed, callback){
+        users[users.indexOf(socket.username)].y-=speed;
+        updatePositions();
+    });
+    socket.on('move down', function(speed, callback){
+        users[users.indexOf(socket.username)].y+=speed;
+        updatePositions();
+    });
+
+    updateUsernames = function(){
+        io.sockets.emit('get users', users);
+        io.sockets.emit('get id', Id);
+    };
+    updatePositions = function(){
+        for(var i=0; i<users.length; i++){
+            if(users[i].x>world.w){
+                users[i].x=world.w;
+            }
+            if(users[i].y>world.h){
+                users[i].y=world.h;
+            }
+            if(users[i].x<0){
+                users[i].x=0;
+            }
+            if(users[i].y<0){
+                users[i].y=0;
+            }
+        }
+        io.sockets.emit('get users', users);
+    };
+    updateMessages = function(){
+        io.sockets.emit('get messages', messages);
+    };
+    updateWorld = function() {
+        io.sockets.emit('update world', world);
+    };
+    ban = function(){
+      for(var i=0; i<banList.length; i++){
+          for(var u=0; u<users.length; u++){
+              if(users[u].ip===banList[i].ip){
+                  io.sockets.emit('banned', users[i].ip);
+              }
+          }
+      }
+    };
+    updateEnemies = function() {
+        io.sockets.emit('update enemies', squares, triangles, pentagons);
+    };
+    if(updatingStarted===false){
+        setInterval(updateEnemies, 0);
+    }
+});
+var collideWith = function(collider, obj){
+
+};
+var collisions = function() {
+    for(var u=0; u<users.length; u++){
+        /* Squares Collisions */
+        for(var i=0; i<squares.length; i++){
+
+        }
+        /* Triangles Collisions */
+        for(var i=0; i<triangles.length; i++){
+
+        }
+        /* Pentagons Collisions */
+        for(var i=0; i<pentagons.length; i++){
+
+        }
+    }
+};
+var enemieUpdates = function(){
+    if(squares.length<world.minimumSquares){
+        squares.push({x: Math.floor(Math.random() * (world.w-100 - 100 + 1) + 100), y: Math.floor(Math.random() * (world.h-100 - 100 + 1) + 100), r: Math.floor(Math.random() * (360 - 0 + 1) + 0)})
+    }
+    if(triangles.length<world.minimumTriangles){
+        triangles.push({x: Math.floor(Math.random() * (world.w-100 - 100 + 1) + 100), y: Math.floor(Math.random() * (world.h-100 - 100 + 1) + 100), r: Math.floor(Math.random() * (360 - 0 + 1) + 0)})
+    }
+    if(pentagons.length<world.minimumPentagons){
+        pentagons.push({x: Math.floor(Math.random() * (world.w-100 - 100 + 1) + 100), y: Math.floor(Math.random() * (world.h-100 - 100 + 1) + 100), r: Math.floor(Math.random() * (360 - 0 + 1) + 0)})
+    }
+    for(var i=0; i<squares.length; i++){
+        squares[i].r+=0.0025;
+    }
+    for(var i=0; i<triangles.length; i++){
+        triangles[i].r+=0.0025;
+    }
+    for(var i=0; i<pentagons.length; i++){
+        pentagons[i].r+=0.0025;
+    }
+    collisions();
+};
+setInterval(enemieUpdates, 0);
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: ' > '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+    var l=line.trim();
+    var msg=l.split(" ");
+    switch(msg[0]){
+        case "playerlist":
+            console.log("Users: ");
+            for(var i=0; i<users.length; i++){
+                console.log("|" + "ID: " + users[i].id + " | " + "Name: " + users[i].name + " | " + "IP: " + users[i].ip);
+            }
+            break;
+
+        case "name":
+            var nameW=[];
+            for(var i=2; i<msg.length; i++){
+                nameW.push(msg[i]);
+            }
+            for(var i=0; i<users.length; i++){
+                if(users[i].id===parseInt(msg[1])){
+                  users[i].name=nameW;
+                  updateUsernames();
+                  console.log("Changed player "+parseInt(msg[1])+"'s name to "+users[i].name);
+                }
+            }
+            break;
+
+        case "chat":
+            if(msg[1]==="all"){
+                var messageW=[];
+                for(var i=2; i<msg.length; i++){
+                    messageW.push(msg[i]);
+                }
+                var message = {msg: messageW.join(" "), user: "[Server]", to: "all"};
+                messages.push(message);
+                updateMessages();
+            } else {
+                var messageW=[];
+                for(var i=2; i<msg.length; i++){
+                    messageW.push(msg[i]);
+                }
+                var message = {msg: messageW.join(" "), user: "[Server]", to: msg[1]};
+                messages.push(message);
+                updateMessages();
+            }
+            break;
+
+        case "chatBan":
+            var cb=false;
+            for(var i=0; i<users.length; i++){
+                if(parseInt(msg[1])===users[i].id){ cb=true; }
+            }
+            for(var i=0; i<chatBanList.length; i++){
+                if(parseInt(msg[1])===chatBanList[i]){ cb=false; }
+            }
+            if(cb){
+                chatBanList.push(parseInt(msg[1]));
+                var message = {msg: "You Have Been Banned From Chatting.", user: "[Server]", to: parseInt(msg[1])};
+                messages.push(message);
+                updateMessages();
+                console.log("Banned Player "+parseInt(msg[1])+" From Chatting.");
+            } else {
+                console.log("Failed To Ban Player "+parseInt(msg[1])+" From Chatting.");
+            }
+            break;
+
+        case "chatBanList":
+            console.log(chatBanList);
+            break;
+
+        case "removeChatBan":
+            for(var i=0; i<chatBanList.length; i++){
+                if(parseInt(msg[1])===chatBanList[i]){
+                    chatBanList.splice(i, 1);
+                    var message = {msg: "You Have Been Unbanned From Chatting.", user: "[Server]", to: parseInt(msg[1])};
+                    messages.push(message);
+                    updateMessages();
+                    console.log("Unbanned Player "+parseInt(msg[1])+" From Chatting.");
+                }
+            }
+            break;
+
+        case "ban":
+            var cb=true;
+            for(var i=0; i<banList.length; i++){
+                if(msg[1]===banList[i].ip){ cb=false; }
+            }
+            console.log(cb);
+            if(cb){
+                for(var i=0; i<users.length; i++){
+                    if(users[i].ip===msg[1]){
+                        banList.push({ip: users[i].ip});
+                        console.log("Banned Player "+msg[1]);
+                        ban();
+                    }
+                }
+            } else {
+                console.log("Failed To Ban Player "+msg[1]);
+            }
+            break;
+
+        case "unBan":
+            for(var i=0; i<banList.length; i++){
+                if(msg[1]===banList[i].ip){
+                    banList.splice(i, 1);
+                    console.log("Unbanned IP "+msg[1]);
+                }
+            }
+            break;
+
+        case "banList":
+            console.log("Banned Users:");
+            console.log(banList);
+            break;
+    }
+    rl.prompt();
+}).on('close', () => {
+  console.log('Bye!');
+  process.exit(0);
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server running On ", addr.address + ":" + addr.port);
+});
