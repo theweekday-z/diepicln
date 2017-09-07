115fad02572f20b9311301841763a6f61bb81ec7
SharkFin <sharkfinpro@hotmail.com>
2017-09-06 17:40:10 -0700

    update
    random rotation directions.
    random movement coming next!

--- src/entities/triangle.js
+++ src/entities/triangle.js
@@ -1,16 +1,17 @@
 const config = require('../core/configService.js').getConfig();
 module.exports = class triangle {
-    constructor(x, y, r, d, vel) {
+    constructor(x, y, r, d, vel, sdir) {
         this.x = x || ~~(Math.random() * (config.w-100 - 100 + 1) + 100);
         this.y = y || ~~(Math.random() * (config.h-100 - 100 + 1) + 100);
         this.r = r || ~~(Math.random() * (360 - 0 + 1) + 0);
         this.d = d || 20;
         this.vel = vel || [0, 0];
         this.hp = 30;
+        this.sdir = sdir || Math.random(0,1) <= 0.5 ? 0.01 : -0.01;
     }
 
     update() {
-        this.r+=0.01;
+        this.r += this.sdir;
         this.x += this.vel[0];
         this.y += this.vel[1];
         this.vel[0] -= this.vel[0] / 20;
